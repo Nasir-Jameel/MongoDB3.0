c3b37df11418a59063f2c1fb5fc9dca4a857ad7b
Nasir-Jameel <beuroairer@gmail.com>
2026-06-20 04:01:12 +0500

    Authentication

--- app.js
+++ app.js
@@ -1,18 +1,29 @@
 const cookieParser = require("cookie-parser")
 const express  = require ("express")
-const bcrypt = require ("bcrypt")
-const jwt = require("jsonwebtoken")
- 
 const app = express()
-app.use(cookieParser())
+const path = require("path")
+const userModel = require("./models/user")
 
-app.get("/",  (req,res)=>{
-    let token = jwt.sign({email: "nasir@example.com"}, "secret");
-    res.cookie("token",token)
-    res.send("done")
+app.set("view engine", "ejs")
+app.use(express.json())
+app.use(express.urlencoded({ extended: true}))
+app.use(express.static(path.join(__dirname, "public")))
+app.use (cookieParser())
+
+app.get("/",(req,res)=>{
+    res.render("index.ejs")
 })
-app.get("/read",(req,res)=>{
-  let data = jwt.verify(req.cookies.token, "secret")
-  console.log(data)
+app.post("/create", async(req,res)=>{
+    let {username , email, password, age} = req.body 
+    let createdUser = await userModel.create({
+   username,
+   email,
+   password,
+   age
+    })
+    res.send(createdUser)
 })
+
+
 app.listen(3000)
+
