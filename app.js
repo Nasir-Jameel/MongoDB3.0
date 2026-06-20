5ced08098c3831a2a0f7ba2cc89d4af63dc3d55a
Nasir-Jameel <beuroairer@gmail.com>
2026-06-20 05:12:18 +0500

    Completed

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const express  = require ("express")
 const app = express()
 const path = require("path")
 const userModel = require("./models/user")
+const bycrypt = require("bcrypt")
+const jwt = require("jsonwebtoken")
 
 app.set("view engine", "ejs")
 app.use(express.json())
@@ -13,16 +15,45 @@ app.use (cookieParser())
 app.get("/",(req,res)=>{
     res.render("index.ejs")
 })
-app.post("/create", async(req,res)=>{
+app.post("/create", (req,res)=>{
     let {username , email, password, age} = req.body 
-    let createdUser = await userModel.create({
+    bycrypt.genSalt(10, (err, salt)=>{
+     bycrypt.hash(password, salt , async (err,  hash)=>{
+        let createdUser = await userModel.create({
    username,
    email,
-   password,
+   password: hash,
    age
     })
+   let token =  jwt.sign({email}, "shhhhhhh")
+   res.cookie("token", token)
     res.send(createdUser)
 })
+   })
+
+    })
+app.get("/login",(req,res)=>{
+    res.render("login.ejs")
+})
+app.post("/login",async (req,res)=>{
+  let user = await userModel.findOne({email: req.body.email})
+  if(!user) return res.send("something went wrong")
+    bycrypt.compare(req.body.password, user.password , function(err,result){
+if (result)
+    {let token =  jwt.sign({email:user.email}, "shhhhhhh")
+   res.cookie("token", token)
+     res.send("yes you can login") 
+}else res.send("no you can not login")
+})
+})
+
+    app.get("/logout",(req,res)=>{
+  res.cookie("token", "")
+  res.redirect("/")
+    })
+  
+
+
 
 
 app.listen(3000)
--- models/user.js
+++ models/user.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose")
 
-mongoose.connect("mongodb://127.0..0.1:27017/AUTHTESTAPP");
+mongoose.connect("mongodb://127.0.0.1:27017/AUTHTESTAPP");
 
 const userSchema = mongoose.Schema({
     username: String,
